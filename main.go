2e1f428c758d742d049dc25e492ee8c9ef19e262
edwinavalos <edwin.a.avalos@gmail.com>
2023-06-01 21:56:49 -0500

    feat(initial): Connect to dht11 sensor

---
+++ main.go
@@ -0,0 +1,34 @@
+package go_dht11_example
+
+import (
+	"fmt"
+	"github.com/d2r2/go-dht"
+	"github.com/stianeikeland/go-rpio/v4"
+	"log"
+)
+
+func main() {
+
+	log.Println("Opening rpio")
+	err := rpio.Open()
+	if err != nil {
+		log.Panicf("could not open rpio: %s", err)
+	}
+	defer rpio.Close()
+
+	//err := logger.ChangePackageLogLevel("dht", logger.FatalLevel)
+	//if err != nil {
+	//	log.Panic().Msgf("could not turn off dht logger: %w", err)
+	//}
+
+	pinNumber := 18
+	tries := 10
+
+	temperature, humidity, _, err := dht.ReadDHTxxWithRetry(dht.DHT11, int(pinNumber), false, tries)
+	if err != nil {
+		log.Fatalf("unable to read sensor after: %d tries err: %s", tries, err)
+	}
+
+	fmt.Printf("temperature: %0.2f°C humidity: %0.2f%%\n", temperature, humidity)
+
+}
